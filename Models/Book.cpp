27bd86b8e748ff05e28dafc0187e83d836263a38
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2022-04-06 23:29:32 +0200

    Rimosse delle chiamate di debug

--- Models/Book.cpp
+++ Models/Book.cpp
@@ -1,9 +1,7 @@
 #include "Book.h"
-#include <QDebug>
 
 Book::Book(const QString& n, const QString& a,int i,unsigned int c):
         title(n), author(a), pubYear(i), quantity(c) {
-    qDebug() << "Book";
 }
 
 QString Book::getTitle() const {
@@ -29,7 +27,3 @@ void Book::setAutor(const QString &a) {author = a;}
 void Book::setPubYear(int i) { pubYear = i;}
 
 void Book::setQuantity(unsigned int q) {quantity = q;}
-
-Book::~Book() {
-    qDebug() << "~Book";
-}
--- Models/Book.h
+++ Models/Book.h
@@ -5,7 +5,7 @@
 class Book {
 public:
     Book(const QString& name = "",const QString& author = "",int year = 0,unsigned int count = 0);
-    virtual ~Book();// = default;
+    virtual ~Book() = default;
 
     QString getTitle() const;
     QString getAuthor() const;
